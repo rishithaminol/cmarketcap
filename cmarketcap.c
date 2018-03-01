c8d20d9f658ea29dd9b0550631ea3874bde09688
Rishitha Minol <rishithaminol@gmail.com>
2018-03-01 14:03:33 +0530

    Data retrievel time optimized

--- cmarketcap.c
+++ cmarketcap.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h> /* time() */
 
 #include "cmarketcap.h"
 #include "json_parser.h"
@@ -10,20 +11,23 @@
 #include "httpd.h"
 
 /* @brief coin_history column map */
-char *col_names[] = {"min_0", "min_5", "min_10", "min_15", "min_20", "min_25", "min_30",
-		"min_35", "min_40", "min_45", "min_50", "min_55", "hr_1",
-		"hr_2", "hr_3", "hr_4", "hr_5", "hr_6", "hr_7", "hr_8", "hr_9",
-		"hr_10", "hr_11", "hr_12", "hr_13", "hr_14", "hr_15", "hr_16",
-		"hr_17", "hr_18", "hr_19", "hr_20", "hr_21", "hr_22", "hr_23",
-		"hr_24"};
+char *col_names[] = {
+	"min_0", "min_5", "min_10", "min_15", "min_20", "min_25", "min_30",
+	"min_35", "min_40", "min_45", "min_50", "min_55", "hr_1",
+	"hr_2", "hr_3", "hr_4", "hr_5", "hr_6", "hr_7", "hr_8", "hr_9",
+	"hr_10", "hr_11", "hr_12", "hr_13", "hr_14", "hr_15", "hr_16",
+	"hr_17", "hr_18", "hr_19", "hr_20", "hr_21", "hr_22", "hr_23",
+	"hr_24"
+};
 
 char *prog_name = NULL; /**< Program name */
 
 /* @brief callback function  */
-void *update_database(void *db_)
+void *__cb_update_database(void *db_)
 {
 	struct coin_entry_base *coin_base;
 	int col_rounds = 0; /**! holds the number of columns currently updated */
+	time_t time_diff;
 
 	sqlite3 *db = (sqlite3 *)db_;
 
@@ -31,6 +35,9 @@ void *update_database(void *db_)
 
 	int i = 0;
 	do {
+		DEBUG_MSG("Starting to count..\n");
+		time_diff = time(NULL); /* === clock starts === */
+
 		if (col_rounds > 0) { /* Executes from the second step of the loop */
 			int j;
 			for (j = col_rounds; j > 0; j--) {
@@ -38,8 +45,6 @@ void *update_database(void *db_)
 				 * shifted
 				 */
 				shift_columns(db, col_names[j], col_names[j - 1]);
-				/* shift_time_stamps(db, ) */
-				DEBUG_MSG("%s shifted to %s\n", col_names[j - 1], col_names[j]);
 			}
 		}
 
@@ -49,7 +54,13 @@ void *update_database(void *db_)
 		DEBUG_MSG("col_rounds = %d\ni = %d\n", col_rounds, i);
 
 		free_entry_base(coin_base);
-		sleep(300);
+		time_diff = time(NULL) - time_diff; /* === clock ends === */
+
+		DEBUG_MSG("%d seconds spent, waiting %d seconds\n", time_diff, (300 - time_diff));
+		if ((300 - time_diff) < 0)
+			CM_ERROR("database update took too much time..\n");
+		else
+			sleep(300 - time_diff);
 	} while (1);
 
 	pthread_exit(NULL);
@@ -58,27 +69,23 @@ void *update_database(void *db_)
 int main(int argc, char *argv[])
 {
 	sqlite3 *db;
-	/*pthread_t update_database_id;
+	pthread_t update_database_id;
 
-	pthread_mutex_init(&sql_db_access, NULL);*/
+	pthread_mutex_init(&sql_db_access, NULL);
 
 	prog_name = *argv;
 
 	db = open_main_db();
+	if (db == NULL) {
+		CM_ERROR("Database error\n");
+		exit(1);
+	}
 
-	shift_columns(db, "min_5", "min_0");
-
-	/*pthread_create(&update_database_id, NULL, update_database, (void *)db);
+	pthread_create(&update_database_id, NULL, __cb_update_database, (void *)db);
 	__cb_main_thread(db);
 
 	close_main_db(db);
 	DEBUG_MSG("main database closed\n");
 
-	pthread_exit(NULL);*/
-
-	/*struct coin_entry_base *sb = new_coin_entry_base();
-	init_coin_history_table(db, sb);*/
-
-	close_main_db(db);
 	return 0;
 } /* main */
