08298b54a3f95f4cd6fd64241c87b20043f7ba8d
Rishitha Minol <rishithaminol@gmail.com>
2018-03-22 21:10:24 +0530

    handle core dump

--- signal_handler.c
+++ signal_handler.c
@@ -7,14 +7,24 @@
 #include "cm_debug.h"
 #include "cmarketcap.h"
 
-void handle_SIGINT(int signal)
+void handle_SIGINT(int sig_num)
 {
 	printf("Caught SIGINT, exiting now\n");
 	close(global_data_handle.httpd_sockfd);
 }
 
+void handle_SIGSEGV(int sig_num)
+{
+	printf("Caught SIGSEGV, exiting now\n");
+	close(global_data_handle.httpd_sockfd);
+	signal(sig_num, SIG_DFL);
+	kill(getpid(), sig_num);
+}
+
 void init_sighandlers()
 {
 	if (signal(SIGINT, handle_SIGINT) == SIG_ERR)
+		CM_ERROR("can't catch SIGINT\n");
+	if (signal(SIGSEGV, handle_SIGSEGV) == SIG_ERR)
 		CM_ERROR("can't catch SIGINT\n");
 }
